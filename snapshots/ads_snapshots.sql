1af4bbd5af1d0f5832f16cb79c977a17aa833bc0
kds-student044 <student044@kahandatasolutions.com>
2023-02-03 19:56:05 +0000

    Create Ads Snapshot and Source

---
+++ snapshots/ads_snapshots.sql
@@ -0,0 +1,14 @@
+{% snapshot ads_snapshot %}
+
+{{
+    config(
+        target_schema= 'snapshots',
+        unique_key = 'ad_id',
+        strategy = 'timestamp',
+        updated_at = 'updated_at'
+    )
+}}
+
+select * from {{ source('ad_platform', 'ads') }}
+
+{% endsnapshot %}
